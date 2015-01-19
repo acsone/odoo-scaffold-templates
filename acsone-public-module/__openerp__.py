6882f091047584e7f8f90bf6ba6360927bf04a93
Laurent Mignon (aka lmi) <laurent.mignon@acsone.eu>
2015-01-19 14:45:53 +0100

    [IMP] move description from __openerp__.py to README.rst

--- acsone-public-module/__openerp__.py
+++ acsone-public-module/__openerp__.py
@@ -28,9 +28,7 @@
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
 
-    'description': """
-        Long description of module's purpose
-    """,
+    # 'description': put the module description in README.rst
 
     'author': "ACSONE SA/NV",
     'website': "http://acsone.eu",
